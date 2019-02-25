6aea19ffe9c80065fcffd2bf390a69b4693dd510
Matthias Küch <mail@matthias-kuech.de>
2019-02-25 23:24:00 +0100

    Initial commit

---
+++ board/board.go
@@ -0,0 +1,85 @@
+package board
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Thread struct {
+	Title          string
+	Link           string
+	Author         string
+	Date           string
+	Answers        int
+	LastAnswerDate string
+	LastAnswerLink string
+}
+
+type Message struct {
+	Content string
+}
+
+func GetMessage(resource string) Message {
+	var m Message
+	doc := getDoc(resource)
+	doc.Find("#norm").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		m.Content = s.Find("font").Text()
+		// t.Link, _ = s.Attr("href")
+		// fmt.Printf("Thread %d: %s - %s\n", i, t.title, t.link)
+	})
+	return m
+}
+
+var boardUrl = "https://www.maniac-forum.de/forum/"
+
+func getDoc(resource string) *goquery.Document {
+	// Request the HTML page.
+	res, err := http.Get(boardUrl + resource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+
+}
+
+func GetThreads() []Thread {
+
+	var threads []Thread
+
+	doc := getDoc("pxmboard.php?mode=threadlist&brdid=1&sortorder=last")
+
+	// Find the review items
+	doc.Find("#threadlist > a").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		var t Thread
+		t.Title = s.Find("font").Text()
+		t.Link, _ = s.Attr("href")
+		// fmt.Printf("Thread %d: %s - %s\n", i, t.title, t.link)
+
+		threads = append(threads, t)
+	})
+
+	doc.Find("#threadlist > font").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		var t = threads[i]
+		t.Author = s.Find("span").Text()
+		// fmt.Printf("Thread %d: %s - %s - %s\n", i, t.Title, t.Link, t.Author)
+
+		threads[i] = t
+	})
+
+	return threads
+}
---
+++ maniacui.go
@@ -0,0 +1,87 @@
+// Copyright 2017 Zack Guo <zack.y.guo@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT license that can
+// be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"lokal/maniacui/board"
+
+	ui "github.com/gizak/termui"
+	"github.com/gizak/termui/widgets"
+)
+
+func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	t := widgets.NewParagraph()
+	t.Title = "Message"
+
+	l := widgets.NewList()
+	l.Title = "Threads"
+
+	threads := board.GetThreads()
+
+	for _, thread := range threads {
+		l.Rows = append(l.Rows, thread.Title)
+	}
+
+	l.TextStyle = ui.NewStyle(ui.ColorYellow)
+	l.WrapText = false
+	l.SetRect(0, 0, 60, 40)
+	t.SetRect(70, 0, 180, 50)
+
+	ui.Render(l)
+	ui.Render(t)
+
+	previousKey := ""
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			l.ScrollDown()
+		case "k", "<Up>":
+			l.ScrollUp()
+		case "<Enter>":
+			// log.Fatalf(threads[l.SelectedRow].Link)
+			message := board.GetMessage(threads[l.SelectedRow].Link)
+			// t.Text = threads[l.SelectedRow].Link
+			t.Text = message.Content
+			// t.Text = "test"
+			ui.Render(t)
+		case "<C-d>":
+			l.HalfPageDown()
+		case "<C-u>":
+			l.HalfPageUp()
+		case "<C-f>":
+			l.PageDown()
+		case "<C-b>":
+			l.PageUp()
+		case "g":
+			if previousKey == "g" {
+				l.ScrollTop()
+			}
+		case "<Home>":
+			l.ScrollTop()
+		case "G", "<End>":
+			l.ScrollBottom()
+		}
+
+		if previousKey == "g" {
+			previousKey = ""
+		} else {
+			previousKey = e.ID
+		}
+
+		ui.Render(l)
+		ui.Render(t)
+
+	}
+}
